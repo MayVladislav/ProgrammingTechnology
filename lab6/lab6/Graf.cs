1cff210d19f05ebf57c05cc97c39d2cfcc051ca4
MayVladislav <56929850+MayVladislav@users.noreply.github.com>
2020-10-17 19:07:04 +0300

    Add files via upload

---
+++ lab6/lab6/Graf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Graf
+    {
+        private double a, b, sum;
+        public double get_b_a() { return b - a; }
+
+
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double Plain()
+        {
+            sum = Math.Pow(b, 2) / 2 - Math.Pow(a, 2) / 2;
+            return sum;
+        }
+    }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+
+            Graf x = new Graf();
+
+            x.Load();
+            Console.WriteLine($"Расстояние от а до b = {x.get_b_a()}");
+            Console.WriteLine($"Плащадь равна: {x.Plain()}");
+
+            Console.SetOut(save_out);
+            new_out.Close();
+            Console.SetIn(save_in);
+            new_in.Close();
+            Console.ReadKey();
+        }
+        
+    }
+}
